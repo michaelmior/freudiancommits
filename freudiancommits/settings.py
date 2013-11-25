a71260391b55119b55447795ee64ed28e06b3bd8
Michael Mior <mmior@uwaterloo.ca>
2013-11-25 16:10:01 -0500

    Don't base static assets off DEBUG setting

--- freudiancommits/settings.py
+++ freudiancommits/settings.py
@@ -169,7 +169,7 @@ LOGGING = {
     }
 }
 
-if not DEBUG:
+if 'AWS_STORAGE_BUCKET_NAME' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
