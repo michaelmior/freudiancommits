c5f4e13ed0a8198366378f8cbfadf303552347fa
Michael Mior <mmior@uwaterloo.ca>
2013-11-25 16:44:34 -0500

    Only try to fetch one issue at a time

--- freudiancommits/github/static/js/randomissue.js
+++ freudiancommits/github/static/js/randomissue.js
@@ -1,12 +1,19 @@
 $(function() {
+  var loading = false;
+
   window.getIssue = function() {
+    if (loading) return;
+    loading = true;
+
     $.ajax({
       'url': '/github/randomissue/',
       'dataType': 'json',
       'success': function(data) {
+        loading = false;
         $('#github-issue').load('/github/issue/' + data.id + '/');
       },
       'error': function() {
+        loading = false;
         $('#github-issue').hide();
         $('.alert-error').show();
       }
